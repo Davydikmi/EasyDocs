c5f0591db3c04e58379fd6fe3750dd7d302d6d6f
Davydikmi_PC <davydovnikita9@gmail.com>
2024-09-24 11:42:36 +0300

    Files now are coping to project's folder

--- Files.xaml.cs
+++ Files.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,25 +24,60 @@ namespace EasyDocs
     /// </summary>
     public partial class Files : Page
     {
+
+        public ObservableCollection<SourceFiles> file_items {  get; set; }
         public Files()
         {
             InitializeComponent();
-            ObservableCollection<FileItem> fileItems = new ObservableCollection<FileItem>
+            file_items = new ObservableCollection<SourceFiles>
             {
-                new FileItem { FileName = "Document1.txt" },
-                new FileItem { FileName = "Presentation.pptx" },
-                new FileItem { FileName = "Image.png" }
+                new SourceFiles { filename = "Document1.txt" },
+                new SourceFiles { filename = "Presentation.pptx" },
+                new SourceFiles { filename = "Image.png" }
             };
-
-            fileListView.ItemsSource = fileItems; // Привязываем данные к ListView
-
+            fileListView.ItemsSource = file_items;
         }
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
+            if (file_items == null)
+            {
+                MessageBox.Show("Коллекция не инициализирована");
+                return;
+            }
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Документы (*.doc; *.docx; *.txt;)|*.doc;*.docx;*.txt";
+            bool? success = fileDialog.ShowDialog();
+            if (success == true)
+            {
+                SourceFiles sourceFiles = new SourceFiles();
+                sourceFiles.filename = fileDialog.SafeFileName;
+                sourceFiles.filepath = fileDialog.FileName;
+                file_items.Add(new SourceFiles { filename = sourceFiles.filename });
+
+                string projectDirectory = Directory.GetCurrentDirectory();
+                string targetDirectory = System.IO.Path.Combine(projectDirectory, "source_files"); 
+
+                // Проверяем, существует ли папка "ProjectFiles", если нет - создаем её
+                if (!Directory.Exists(targetDirectory))
+                {
+                    Directory.CreateDirectory(targetDirectory);
+                }
+
+                // Определяем целевой путь, куда скопировать файл
+                string targetFilePath = System.IO.Path.Combine(targetDirectory, sourceFiles.filename);
+                File.Copy(sourceFiles.filepath, targetFilePath, overwrite: true);
+            }
         }
-        private void Delete_Click(object sender, RoutedEventArgs e) { }
-        private void Reset_Click(object sender, RoutedEventArgs e) { }
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+        
+        }
+        private void Reset_Click(object sender, RoutedEventArgs e) 
+        { 
+        
+        }
     }
     public class FileItem
     {
---
+++ SourceFiles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyDocs
+{
+    public class SourceFiles
+    {
+        public string filename { get; set; }
+        public string filepath { get; set; }
+    }
+}
