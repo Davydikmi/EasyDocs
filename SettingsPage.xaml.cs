0442f7d5756ffc630211cf58c1175a8377c8ed0a
Davydikmi_laptop <davydovnikita9@gmail.com>
2024-11-28 14:35:48 +0300

    Сериализация данных из Markers.json в окно настроек

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -47,6 +49,67 @@ namespace EasyDocs
             }
         }
         //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        private void ChangeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PageLoaded(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(TextReplacer.filepath))
+            {
+                try
+                {
+                    string jsonData = File.ReadAllText(TextReplacer.filepath);
+
+                    TextReplacer markers = JsonConvert.DeserializeObject<TextReplacer>(jsonData);
+
+                    if (markers != null)
+                    {
+                        FIOMarkerTextBox.Text = markers.FIO_marker;
+                        phoneNumberMarkerTextBox.Text = markers.phone_numb_marker;
+                        birthDateMarkerTextBox.Text = markers.birth_date_marker;
+                        adressMarkerTextBox.Text = markers.adress_marker;
+                        passportMarkerTextBox.Text = markers.passport_SeriesNumb_marker;
+                        idNumberMarkerTextBox.Text = markers.id_numb_marker;
+
+                        switch(markers.bracket_type)
+                        {
+                            case "[]":
+                                square_brackets.IsChecked = true;
+                                break;
+                            case "{}":
+                                brace.IsChecked = true;
+                                break;
+                            case "//":
+                                slash_brackets.IsChecked = true;
+                                break;
+                            case "<>":
+                                angle_brackets.IsChecked = true;
+                                break;
+                            default:
+                                MessageBox.Show("Ошибка","Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                                return;
+                        }
+                    }
+                    else return;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+            else
+            {
+                StreamWriter writer = new StreamWriter(TextReplacer.filepath);
+                writer.Close();
+            }
+        }
+
+
+
 
     }
 }
